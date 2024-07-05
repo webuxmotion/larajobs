6edede3db05667b063093964d279f390984fc62e
webuxmotion <pereverziev.andrii@gmail.com>
2024-07-05 14:35:02 +0300

    Create listing

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ListingController::class, 'index']);
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+Route::post('/listings', [ListingController::class, 'store']);
+Route::get('/create-listing', [ListingController::class, 'create']);
